2273c9404e1d5ff9c70d596fae6b364251287a0a
Dustin Page <dustinpage@live.com>
2018-01-16 16:40:55 -0600

    Add Mocha, Chai  and JSDOM for running JS tests

---
+++ src/index.test.js
@@ -0,0 +1,24 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+
+describe ('Our first test', () => {
+	it('should pass', () => {
+		expect(true).to.equal(true);
+	});
+});
+
+describe('index.html', () => {
+	it('should say hello', (done) => {
+		const index = fs.readFileSync('./src/index.html', "utf-8"); //Places the context of the html file in-memory in the "index" constant
+
+		jsdom.env(index, (err, window) => { //Note: The callback here makes this an async test so you need to use the "done()" method //Pass "index" to jsdom.env to create a virtual DOM in memory
+			const h1 = window.document.getElementsByTagName('h1')[0];
+
+			expect(h1.innerHTML).to.equal("Hello World!");
+			done(); //Need to tell Mocha the test is done and then it will run the expect
+			window.close(); //Close the window to free up the memory that was taken when we created the in-memory DOM
+		});
+	});
+});
