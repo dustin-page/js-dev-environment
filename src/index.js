b779c73a763af2260f168a7d4d79475f6e6e3c81
Dustin Page <dustinpage@live.com>
2018-01-16 15:03:21 -0600

    Add ESLint to development build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.css';
 
 import numeral from 'numeral'; //numeral is a handy library for formatting numbers
 
-const courseValue = numeral(1000).format('$0,0.00');
+const courseValue = numeral(1000).format('$0,0.00'); // eslint-disable-line no-unused-vars
+
 //debugger; //Debug breakpoint
-console.log(`I would pay ${courseValue} for this awesome course!`);
+//console.log(`I would pay ${courseValue} for this awesome course!`);
