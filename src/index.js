9b135cfb39c8d657700ee9458b069b8925a99ba7
Dustin Page <dustinpage@live.com>
2018-01-17 09:58:02 -0600

    Set up app to handle HTTP calls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import './index.css';
+import {getUsers} from './api/userApi';
 
-import numeral from 'numeral'; //numeral is a handy library for formatting numbers
+getUsers().then( result => {
 
-const courseValue = numeral(1000).format('$0,0.00'); // eslint-disable-line no-unused-vars
+	let usersBody = "";
 
-//debugger; //Debug breakpoint
-//console.log(`I would pay ${courseValue} for this awesome course!`);
+	result.forEach( user => {
+		usersBody += `<tr>
+		<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+		<td>${user.id}</td>
+		<td>${user.firstName}</td>
+		<td>${user.lastName}</td>
+		<td>${user.email}</td>
+		</tr>`
+	});
+
+	global.document.getElementById('users').innerHTML = usersBody;
+});
--- src/index.test.js
+++ src/index.test.js
@@ -10,13 +10,13 @@ describe ('Our first test', () => {
 });
 
 describe('index.html', () => {
-	it('should say hello', (done) => {
+	it('should have h1 that says Users', (done) => {
 		const index = fs.readFileSync('./src/index.html', "utf-8"); //Places the context of the html file in-memory in the "index" constant
 
 		jsdom.env(index, (err, window) => { //Note: The callback here makes this an async test so you need to use the "done()" method //Pass "index" to jsdom.env to create a virtual DOM in memory
 			const h1 = window.document.getElementsByTagName('h1')[0];
 
-			expect(h1.innerHTML).to.equal("Hello World!");
+			expect(h1.innerHTML).to.equal("Users");
 			done(); //Need to tell Mocha the test is done and then it will run the expect
 			window.close(); //Close the window to free up the memory that was taken when we created the in-memory DOM
 		});
